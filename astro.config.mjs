3a67a2caa3126a5ad172241f147b10fa1bfe2dc1
Edu Ruiz <eruiz@avallain.com>
2024-02-15 20:44:47 +0100

    detail page added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   prefetch: {
-    defaultStrategy: 'load'
+    defaultStrategy: 'viewport'
   }
 });
